f56f99caee5027aaaa50136023dd22ea3dfb873c
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2020-07-03 02:38:39 +0530

    Updated tfrecorder_explorer

--- scripts/tfrecord_explorer.py
+++ scripts/tfrecord_explorer.py
@@ -1,10 +1,9 @@
 import argparse
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import os
-import random 
 import tensorflow as tf
+
 
 def read_examples(tfrecord_file):
     '''Returns a list of examples read from a single tfrecord'''
@@ -14,10 +13,15 @@ def read_examples(tfrecord_file):
     return examples
 
 
-def wrap_around(images):
+def slideshow(images):
+    '''
+    Takes in a list of tuples.
+    0th value of tuple is an RGB image.
+    1st value of tuple is the label.
+    '''
     num_img = len(images)
     current = 0
-    timeout = 1000
+    timeout = 5000
     window_size = (1280, 720)
     
     print('Number of Examples:', num_img)
@@ -26,20 +30,19 @@ def wrap_around(images):
     finish = False
     while not finish:
         title = images[current][1]
-        image = cv2.cvtColor(images[current][0], cv2.COLOR_RGB2BGR)
+        print('Current Image:', current, 'Label:', title)
         
+        image = cv2.cvtColor(images[current][0], cv2.COLOR_RGB2BGR)
         cv2.namedWindow(title, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(title, window_size[0], window_size[1])
         cv2.imshow(title, image)
-        print('\rCurrent Image:', current, end='')
-        
         key = cv2.waitKey(timeout)
-        print("Key:", key, '\tClass:', type(key))
-        
-        #print("ord(Key):", ord(key))
 
         if key == ord('n') or key == -1:
-            current = (current + 1) % num_img
+            if current == num_img - 1:
+                finish = True
+            else:
+                current = (current + 1) % num_img
         elif key == ord('p'):
             if current == 0:
                 current = num_img - 1
@@ -48,12 +51,11 @@ def wrap_around(images):
         else:
             finish = True
         cv2.destroyAllWindows()
-    print()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='View images in a tfrecord')
-    parser.add_argument('tfrecord', help='Location of tfrecord')
+    parser.add_argument('--tfrecord', help='Location of tfrecord')
     args = parser.parse_args()
     
     tfrecord = args.tfrecord
@@ -69,10 +71,7 @@ if __name__ == "__main__":
         img = cv2.imdecode(img_np_arr, 1)
         
         im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        images.append((im_rgb[:256, :480], label))
-        '''
-        plt.title(label)
-        plt.imshow(img)
-        plt.show()
-        '''
-    wrap_around(images)
+        # images.append((im_rgb[:256, :480], label))
+        images.append((im_rgb, label))
+        
+    slideshow(images)
