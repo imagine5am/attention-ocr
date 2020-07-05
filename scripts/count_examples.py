aa23ed1373d1367b509c8c31bdef1616262d6227
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2020-07-06 00:56:07 +0530

    Added count examples script

---
+++ scripts/count_examples.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+import tensorflow as tf
+
+def count_examples(tfrecord_file):
+    '''Returns count of number of examples in a tfrecord'''
+    count = 0
+    for _ in tf.python_io.tf_record_iterator(tfrecord_file):
+        count += 1
+    return count
+
+
+def list_files(src_loc, file_extension):
+    '''List files in src_loc and returns files which have the valid file_extension'''
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(src_loc):
+        for file in f:
+            if file.endswith(file_extension):
+                files.append(os.path.join(r, file))
+    return files
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Counts number of examples.')
+    parser.add_argument('--tfrecords_loc', help='Location of tfrecords.')
+    args = parser.parse_args()
+    
+    tfrecords_loc = args.tfrecords_loc
+    tfrecords = list_files(src_loc=tfrecords_loc, file_extension='.tfrecords')
+    count = 0
+    for tfrecord in tfrecords:
+        count += count_examples(tfrecord)
